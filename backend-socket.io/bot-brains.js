3cc8efb253ad9779fe2b3b3fc3b656030f97d9cd
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2023-03-05 21:55:15 +0400

    Add function to modify answers

--- backend-socket.io/bot-brains.js
+++ backend-socket.io/bot-brains.js
@@ -126,3 +126,91 @@ function bubbleSort(text, arr, property) {
     }
     return arr;
 }
+function modifyAnswer(keyword_tag, answ, num = 0) {
+    // replacing [info]
+    switch (keyword_tag) {
+        case "pizza":
+            return answ.replace(/\[.*]/g, pizzaModifiers.type.map((elem) => {
+                return (" " + (elem.charAt(0).toUpperCase() + elem.slice(1)))
+            }))
+        case "location":
+            return answ.replace(/\[.*]/g, details.address)
+        case "our_address":
+            answ = answ.replace(/\[restaurant_location]/g, details.address)
+            return answ.replace(/\[min]/g, Math.floor(Math.random() * 41) + 20)
+        case "user_address_answ":
+            return answ.replace(/\[.*]/g, Math.floor(Math.random() * 41) + 20)
+        case "contact":
+            answ = answ.replace(/\[email]/g, details.email)
+            return answ.replace(/\[.*]/g, details.telephone)
+        case "social media":
+
+            let organized_answer = "\n"
+            for (let media in details.social_media) {
+                organized_answer += "-> " + (media.charAt(0).toUpperCase() + media.slice(1)) + ": "
+                    + details.social_media[media] + "\n"
+            }
+            return answ.replace(/\[.*]/g, organized_answer)
+        case "delivery":
+            return answ.replace(/\[.*]/g, details.delivery_map_link)
+        case "menu":
+            let str = ""
+            str = "[Large, standard, small]: "
+            for (let cost_type in pizzaMenu) {
+                str += `[${pizzaMenu[cost_type].size["Large"]}$, ${pizzaMenu[cost_type].size["Standard"]}$, ` +
+                    `${pizzaMenu[cost_type].size["Small"]}$] -> `
+                for (let pizza_type of pizzaMenu[cost_type].type) {
+                    str += (pizza_type.charAt(0).toUpperCase() + pizza_type.slice(1)) + ", "
+                }
+                str = str.slice(0, -2) + "; "
+            }
+            return answ.replace(/\[.*]/g, str.slice(0, -2))
+        case "sauce":
+            return answ.replace(/\[.*]/g, pizzaModifiers.sauce.map((elem) => {
+                return (" " + (elem.charAt(0).toUpperCase() + elem.slice(1)))
+            }))
+        case "weight":
+            answ = answ.replace(/\[large]/g, variants.weight.large.toString())
+            answ = answ.replace(/\[standard]/g, variants.weight.standard.toString())
+            return answ.replace(/\[small]/g, variants.weight.small.toString())
+        case "ready_pizza":
+            answ = answ.replace(/\[size]/g, pizza_lst[num].size)
+            answ = answ.replace(/\[type]/g, pizza_lst[num].type)
+            answ = answ.replace(/\[crust]/g, pizza_lst[num].crust)
+            return answ.replace(/\[sauce]/g, pizza_lst[num].sauce)
+        case "order_number":
+            let capital_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            let number = ""
+            for (let i = 0; i < 7; i++) {
+                number += capital_letters.charAt(Math.floor(Math.random() * capital_letters.length));
+            }
+            number += "#" + Math.floor(Math.random() * 8998 + 1001)
+            return answ.replace(/\[.*]/g, number)
+        case "overall_price":
+            return answ.replace(/\[.*]/g, getOverallPrice().toString())
+        case "current_order":
+            let current_pizzas = "";
+            for (let i = 0; i < pizza_lst.length; i++) {
+                current_pizzas += "\n--> " + pizza_lst[i].size + " " + pizza_lst[i].type + " pizza"
+                if (pizza_lst[i].crust.length !== 0)
+                    current_pizzas += " on " + pizza_lst[i].crust + " crust"
+                if (pizza_lst[i].sauce.length !== 0)
+                    current_pizzas += " with " + pizza_lst[i].sauce + " sauce"
+            }
+            if (current_pizzas === "")
+                answ = answ.replace(/\[.*]/g, randomizeAnswers(variants.current_order))
+            return answ.replace(/\[.*]/g, current_pizzas)
+        case "help":
+            let help_list = ""
+            for (let interaction of variants.help)
+                help_list += interaction
+            return answ.replace(/\[.*]/g, help_list)
+        case "reviews":
+            answ = answ.replace(/\[.*]/g, "")
+            for (let i = 0; i < 3; i++)
+                answ += variants.reviews[Math.floor(Math.random() * variants.reviews.length)]
+            return answ
+        default:
+            return answ
+    }
+}
